23d9afd150ccde487be1cfed6cece54a608b6231
denisshashkov <shashkov-56@mail.ru>
2021-12-18 15:36:56 +0300

    changed todo limit

--- src/API/PostService.jsx
+++ src/API/PostService.jsx
@@ -4,7 +4,7 @@ export default class PostService {
   static async getAll() {
     try {
       const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos"
+        "https://jsonplaceholder.typicode.com/todos?_limit=30"
       );
       return response.data;
     } catch (error) {
